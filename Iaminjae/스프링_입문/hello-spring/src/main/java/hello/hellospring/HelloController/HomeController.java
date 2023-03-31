7f3870a36836c6e4e8ec785597641a5894acfe1b
minjae <wjsalswp303@gmail.com>
2023-03-31 15:52:08 +0900

    [Feat(마루/전민제): 강의 현황 업데이트]

--- Iaminjae/스프링_입문/hello-spring/src/main/java/hello/hellospring/HelloController/HomeController.java
+++ Iaminjae/스프링_입문/hello-spring/src/main/java/hello/hellospring/HelloController/HomeController.java
@@ -1,2 +1,14 @@
-package hello.hellospring.HelloController;public class HomeController {
+package hello.hellospring.HelloController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String Home(){
+        return "home";
+    }
+
 }
