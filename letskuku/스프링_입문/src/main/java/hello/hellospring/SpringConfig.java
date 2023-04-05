f68fd565271d8a27e19208cc2548779b7354576a
letskuku <yujung9288@naver.com>
2023-04-06 05:17:56 +0900

    JPA 환경설정

--- letskuku/스프링_입문/src/main/java/hello/hellospring/SpringConfig.java
+++ letskuku/스프링_입문/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,24 +1,24 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcMemberRepository;
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
     private final DataSource dataSource;
+    private final EntityManager em;
 
     @Autowired
-    public SpringConfig(DataSource dataSource) {
+    public SpringConfig(DataSource dataSource, EntityManager em) {
         this.dataSource = dataSource;
+        this.em = em;
     }
 
     @Bean
@@ -30,6 +30,7 @@ public class SpringConfig {
     public MemberRepository memberRepository() {
 //      return new MemoryMemberRepository();
 //      return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
+//      return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
