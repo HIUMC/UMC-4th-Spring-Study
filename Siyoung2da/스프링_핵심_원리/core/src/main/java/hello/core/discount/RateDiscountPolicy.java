53586e220cb8f47cde737699ed296fb8c17ff72c
Siyoung Park <drq5@daum.net>
2023-05-03 15:37:11 +0900

    [Feat(Siyoung2da)] : 스프링 핵심원리

---
+++ Siyoung2da/스프링_핵심_원리/core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,26 @@
+package hello.core.discount;
+
+import hello.core.annotation.MainDiscountPolicy;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+//@Qualifier("mainDiscountPolicy")  Qualifier 끼리 사용 -> 못 찾으면 Bean찾음 -> 그것도 못 찾으면 에러
+//@Primary  우선순위로 조회
+@MainDiscountPolicy
+public class RateDiscountPolicy implements DiscountPolicy {
+
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
